c79e1d7dc13acb81e47cdafff839b9f5fe0612bc
skalaa7 <skalaandrej7@gmail.com>
2021-03-14 21:13:07 +0100

    funkcija _delay_ms()

--- Vezba2/Zadatak1/zadatak1/main.c
+++ Vezba2/Zadatak1/zadatak1/main.c
@@ -1,8 +1,16 @@
-/*
- * main.c
- *
- *  Created on: Mar 14, 2021
- *      Author: Andrej
- */
+#include <avr/io.h>
+#include <util/delay.h>
+int main()
+{
+ DDRB |= 1 << 5; //PB5 je izlaz
+ while (1)
+ {
+ PORTB |= 1 << 5; //LED ON
+ _delay_ms(1000); //pauza 1s
+ PORTB &= ~(1 << 5); //LED OFF
+ _delay_ms(1000); //pauza 1s
+ }
+ return 0;
+}
 
 
