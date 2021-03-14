f4899ccd641d68a6c4f111e2adbf2cd81abb1a73
skalaa7 <skalaandrej7@gmail.com>
2021-03-14 21:29:46 +0100

    pomocu Tajmera 0

--- Vezba2/Zadatak1/zadatak1/main.c
+++ Vezba2/Zadatak1/zadatak1/main.c
@@ -1,16 +1,33 @@
 #include <avr/io.h>
-#include <util/delay.h>
+#include <avr/interrupt.h>
+unsigned long milliss = 0;
+ISR(TIMER0_COMPA_vect)
+{
+ milliss++; //sistemsko vreme
+}
 int main()
 {
- DDRB |= 1 << 5; //PB5 je izlaz
- while (1)
+ unsigned long t0;
+ DDRB |= 1 << 5;
+
+
+ //inicijalizacija tajmera 0:
+ TCCR0A = 0x02; //tajmer 0: CTC mod
+ TCCR0B = 0x03; //tajmer 0: fclk = fosc/64
+ OCR0A = 249; //perioda tajmera 0: 250 Tclk (OCR0A + 1 = 250)
+ TIMSK0 = 0x02; //dozvola prekida tajmera 0
+ //usled dostizanja vrednosti registra OCR0A
+ sei(); //I = 1 (dozvola prekida)
+
+ while(1)
  {
- PORTB |= 1 << 5; //LED ON
- _delay_ms(1000); //pauza 1s
- PORTB &= ~(1 << 5); //LED OFF
- _delay_ms(1000); //pauza 1s
+ PORTB |= 1 << 5;
+ t0 = milliss;
+ while ((milliss - t0) < 1000); //pauza 500ms
+ PORTB &= ~(1 << 5);
+ t0 = milliss;
+ while ((milliss - t0) < 1000); //pauza 500ms
  }
  return 0;
 }
-
 
